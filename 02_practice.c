efef8c025b1c21438ad094d514bec9786f13c5ca
abhinavSingh <abhi.anu003@gmail.com>
2020-12-30 11:00:08 +0530

    more confusion comes here

--- 02_practice.c
+++ 02_practice.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main(){
-    // if a nummber is divisible by 97 or not
     int num;
     printf("Enter the number\n");
     scanf("%d", &num);
     printf("Divisibility test returns : %d\n", num%97);
-    getch();
 }
